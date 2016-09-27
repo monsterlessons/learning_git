7cc07c5fd4b144a12a719d6f9aefc02ae98892d3
Alexander Kochergin <alexander.kochergin@aboutyou.de>
2016-09-27 21:53:23 +0200

    Implemented auth

---
+++ auth.js
@@ -0,0 +1,7 @@
+function authenticate (login, password) {
+  if (login === 'login' && password === 'password') {
+    return 'You were logged in';
+  } else {
+    return 'Login is incorrect';
+  }
+}
