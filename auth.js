41dab35dc070e095052e26d8ba5dd52a94c61bf3
Alexander Kochergin <alexander.kochergin@aboutyou.de>
2016-10-04 22:11:41 +0200

    Changed auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 function authenticate (login, password) {
-  if (login === 'login' && password === 'password') {
+  if (login === 'login') {
     return 'You were logged in';
   } else {
-    return 'Login is incorrect';
+    return 'Login is correct';
   }
 }
